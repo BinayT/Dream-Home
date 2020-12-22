6ba3e4e98914ba8ac333d5027b6e0bacebc5909e
Binay <binayprogramming@gmail.com>
2020-12-22 19:29:19 +0100

    Next slide's logic set

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
 import Slide from './Slide/Slide';
 import './Slider.scss';
 
 function Slider({ slides }) {
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+  const timeout = useRef(null);
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+    console.log(current);
+  };
+
   return (
     <section className='slider'>
       <div className='slider__wrapper'>
@@ -14,7 +23,7 @@ function Slider({ slides }) {
       </div>
       <div className='slider__buttons'>
         <FaArrowLeft className='arrow__buttons' />
-        <FaArrowRight className='arrow__buttons' />
+        <FaArrowRight className='arrow__buttons' onClick={nextSlide} />
       </div>
     </section>
   );
